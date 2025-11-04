107c66b942ddc7292ddb1a8dc2b9124394a84d09
giuseppefani1978-cell <giuseppe.fani1978@gmail.com>
2025-11-04 19:28:48 +0100

    Update battle.js

--- src/battle.js
+++ src/battle.js
@@ -167,7 +167,7 @@ function __redirectAfterWin(foeType){
     location.href = withBase('app.html#/poi/gallipoli/realmap');
   } else {
     // default (Otranto or unknown) → Bonus hub
-    location.href = withBase('app.html#/poi/otranto/realmap');
+    location.href = withBase('index.html?embed=1#/poi/otranto/realmap');
   }
   window.dispatchEvent(new CustomEvent('app:navigate', { detail:{ to: 'bonus' }}));
 }
